7a0a6937dbeeebb9cc41231e87adfdfba9d742ae
ind0zer <indozer@proton.me>
2025-12-12 12:58:33 +0200

    123123

--- src/redux/operations.jsx
+++ src/redux/operations.jsx
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://690a7cea1a446bb9cc22b0b1.mockapi.io/contacts/contacts";
+axios.defaults.baseURL = "https://690a7cea1a446bb9cc22b0b1.mockapi.io/contacts";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
